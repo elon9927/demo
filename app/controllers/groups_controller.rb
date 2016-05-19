9867ff15f600bda623ddb8b31b4bf03c588ff874
elon <elon@sinfotech.cn>
2016-05-19 22:12:27 +0800

    crud finish

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -16,6 +16,22 @@ class GroupsController < ApplicationController
 			render :new
 		end
 	end
+	def edit
+		@group = Group.find(params[:id])
+	end
+	def update
+		@group = Group.find(params[:id])
+		if @group.update(group_params)
+			redirect_to group_path(@group)
+		else
+			render :edit
+		end
+	end
+	def destroy
+		@group = Group.find(params[:id])
+		@group.destroy
+		redirect_to groups_path
+	end
 
 
 
